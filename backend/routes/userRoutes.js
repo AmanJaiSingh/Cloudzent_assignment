8f9998fca2845ba702927be865153bd0c836d241
AmanJaiSingh <76746180+AmanJaiSingh@users.noreply.github.com>
2026-03-11 03:39:14 +0530

    fix: export and route /me user endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 
-const { getUsers } = require("../controllers/userController");
+const { getUsers, getMe } = require("../controllers/userController");
 
 router.get("/", auth, getUsers);
+router.get("/me", auth, getMe);
 
 module.exports = router;
